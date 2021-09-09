e3a255a4fbadcd7e0a348d170380094691aede8d
imanonion <79980714+imanonion@users.noreply.github.com>
2021-09-09 22:55:49 +0800

    completed onboard welcome page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import * as eva from "@eva-design/eva";
+import { default as theme } from "./custom-theme.json";
+import { ApplicationProvider } from "@ui-kitten/components";
+import { OnboardWelcome } from "./src/navigation/MainNavigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default () => (
+  <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+      <OnboardWelcome />
+  </ApplicationProvider>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/navigation/MainNavigator.tsx
@@ -0,0 +1,4 @@
+import OnboardWelcome from "../screens/onboarding/OnboardWelcome";
+import OnboardOne from "../screens/onboarding/OnboardOne";
+
+export {OnboardWelcome, OnboardOne}
