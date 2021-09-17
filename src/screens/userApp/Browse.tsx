60d004beef795f53f09435ea6cbe64b443bb128f
imanonion <79980714+imanonion@users.noreply.github.com>
2021-09-17 23:28:38 +0800

    created cards in list

--- src/screens/userApp/Browse.tsx
+++ src/screens/userApp/Browse.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, FunctionComponent } from "react";
 import { StyleSheet } from "react-native";
-import { Button, Layout, Text, Divider, Input, List, ListItem } from "@ui-kitten/components";
+import { Button, Layout, Text, Card, List, ListItem } from "@ui-kitten/components";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { AppStackParamList } from "./AppStackParams";
 import { AuthenticatedUserContext } from "../../navigation/AuthenticatedUserProvider";
@@ -8,11 +8,13 @@ import { AuthenticatedUserContext } from "../../navigation/AuthenticatedUserProv
 import { Firebase } from "../../services/Firebase";
 
 type Props = {
-  item: {genre: string},
-  index: number
+  item: {genre: string, description: string},
 }
 
-const genreList = [{"genre": "Hip Hop"}, {"genre": "Breaking"}]
+const genreList = [
+  {"genre": "Hip Hop", "description": "1 out of 2 completed"},
+  {"genre": "Breaking", "description": "1 out of 2 completed"}
+]
 
 // let hipHopInfo = Firebase.firestore().collection("lessons").where("genre", "==", "Hip Hop")
 // console.log(hipHopInfo)
@@ -28,17 +30,16 @@ const Browse = () => {
   // }
   
 
-  const renderItem = ({item, index}: Props) => (
-    <ListItem 
-      title={`${item.genre} ${index + 1}`}
-    />
+  const renderItem = ({item}: Props) => (
+    <Card status="primary" style={styles.cardStyle}>
+      <Text style={styles.genreStyle}>{`${item.genre}`}</Text>
+      <Text>{`${item.description}`}</Text>
+      <Button style={styles.button}>{'>'}</Button>
+    </Card>
   )
 
   return (
     <>
-      <Layout>
-        <Text>hey</Text>
-      </Layout>
       <List 
         style={styles.listStyle}
         data={genreList}
@@ -58,6 +59,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   listStyle: {
-    maxHeight: 180
+    
+  },
+  cardStyle: {
+    marginBottom: 5
+  },
+  genreStyle: {
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  button: {
+    width:48,
+    height: 48,
+    alignSelf: "flex-end",
+    borderRadius: 50,
+
   }
 });
