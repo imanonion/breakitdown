48e8c61ba09950c06f15148c07d7927bbb71d0d5
imanonion <79980714+imanonion@users.noreply.github.com>
2021-09-17 22:49:44 +0800

    added listitem

--- src/screens/userApp/Browse.tsx
+++ src/screens/userApp/Browse.tsx
@@ -1,19 +1,51 @@
 import React, { useContext, FunctionComponent } from "react";
 import { StyleSheet } from "react-native";
-import { Button, Layout, Text, Divider, Input } from "@ui-kitten/components";
+import { Button, Layout, Text, Divider, Input, List, ListItem } from "@ui-kitten/components";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { AppStackParamList } from "./AppStackParams";
 import { AuthenticatedUserContext } from "../../navigation/AuthenticatedUserProvider";
 
 import { Firebase } from "../../services/Firebase";
 
-const Browse: FunctionComponent = () => {
+type Props = {
+  item: {genre: string},
+  index: number
+}
+
+const genreList = [{"genre": "Hip Hop"}, {"genre": "Breaking"}]
+
+// let hipHopInfo = Firebase.firestore().collection("lessons").where("genre", "==", "Hip Hop")
+// console.log(hipHopInfo)
+
+const Browse = () => {
   const { user } = useContext(AuthenticatedUserContext);
 
+  // try {
+  //   let hipHopInfo = await Firebase.firestore().collection("lessons").where("genre", "==", "Hip Hop")
+  //   console.log(hipHopInfo)
+  // } catch (err) {
+  //   console.log(`error getting hip hop collection: ${err}`)
+  // }
+  
+
+  const renderItem = ({item, index}: Props) => (
+    <ListItem 
+      title={`${item.genre} ${index + 1}`}
+    />
+  )
+
   return (
-    <Layout style={styles.container}>
-      <Text>BROWSE LESSONS</Text>
-    </Layout>
+    <>
+      <Layout>
+        <Text>hey</Text>
+      </Layout>
+      <List 
+        style={styles.listStyle}
+        data={genreList}
+        renderItem={renderItem}
+      />
+    </>
+
   );
 };
 
@@ -25,4 +57,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  listStyle: {
+    maxHeight: 180
+  }
 });
